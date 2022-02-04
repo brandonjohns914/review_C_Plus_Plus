b0de76f6ed10fdc5e16375cb830bead3b12e052f
Brandon Johns <brandonjohns914@yahoo.com>
2022-02-03 16:35:15 -0800

    Initial Commit

---
+++ review_C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  review_C++
+//
+//  Created by Brandon Johns on 2/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
